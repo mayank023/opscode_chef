c5d29121c71f41d91c8227d832b3a53d557fd94a
“mayaverm” <mayaverm@in.ibm.com>
2017-01-25 22:42:31 +0530

    added syntax test

--- chef-repo/cookbooks/webserver_test/recipes/default.rb
+++ chef-repo/cookbooks/webserver_test/recipes/default.rb
@@ -5,28 +5,24 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 package_name =
-  service_name =
-    case node['platform']
-    when 'centos' then 'httpd'
-    when 'ubuntu' then 'apache2'
-    end
-
+    service_name =
+        case node['platform']
+        when 'centos' then 'httpd'
+        when 'ubuntu' then 'apache2'
+        end
 
 package package_name do
-  action :install
+    action :install
 end
 
 service service_name do
-  action [:start, :enable]
+    action [:start, :enable]
 end
 
 file '/var/www/html/index.html' do
-  content '<html>
+    content '<html>
   <body>
     <h1>hello world</h1>
   </body>
 </html>'
 end
-
-
-
